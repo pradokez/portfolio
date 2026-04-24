66f58625117ecd3e102dc36963f637c10878e4fe
Keziah <keziahprado@MacBook-Air-de-Keziah.local>
2026-04-24 16:34:58 -0300

    fix(profile): restore yellow heading, add View GitHub button, move LevelProgress to own section

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
 import ProfileSection from './components/ProfileSection'
+import LevelProgress from './components/LevelProgress'
 import TechStackSection from './components/TechStackSection'
 import QuestLogSection from './components/QuestLogSection'
 import CareerSection from './components/CareerSection'
 import ContactSection from './components/ContactSection'
+import { projects } from './data/projects'
 
 export default function App() {
+  const totalProjects = projects.length
+  const projectsCompleted = projects.filter((p) => p.level >= 2).length
+
   return (
     <div className="min-h-screen bg-surface text-on-surface">
       <header>
         <ProfileSection />
       </header>
+      <section className="px-4 pb-12 max-w-3xl mx-auto">
+        <LevelProgress
+          level={7}
+          xp={2100}
+          xpToNext={2500}
+          projectsCompleted={projectsCompleted}
+          totalProjects={totalProjects}
+        />
+      </section>
       <main>
         <TechStackSection />
         <QuestLogSection />
